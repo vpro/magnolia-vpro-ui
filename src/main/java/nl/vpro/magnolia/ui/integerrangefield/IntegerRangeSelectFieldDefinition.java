6fc7b8fb1bd245207313757757e5666bae544044
Michiel Meeuwissen <git@meeuw.org>
2022-01-20 15:51:21 +0100

    integer range support

---
+++ src/main/java/nl/vpro/magnolia/ui/integerrangefield/IntegerRangeSelectFieldDefinition.java
@@ -0,0 +1,62 @@
+package nl.vpro.magnolia.ui.integerrangefield;
+
+import info.magnolia.objectfactory.ComponentProvider;
+import info.magnolia.ui.field.ConfiguredFieldDefinition;
+import info.magnolia.ui.field.FieldType;
+import info.magnolia.ui.field.factory.AbstractFieldFactory;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.stream.IntStream;
+
+import com.vaadin.ui.*;
+
+/**
+ * @author Michiel Meeuwissen
+ * @since 1.0
+ * */
+@Getter
+@Setter
+@FieldType("integerRangeField")
+public class IntegerRangeSelectFieldDefinition extends ConfiguredFieldDefinition<Integer> {
+
+    private int min = 0;
+    private int max = 10;
+
+    public IntegerRangeSelectFieldDefinition() {
+        setFactoryClass(Factory.class);
+        setType(Integer.class);
+    }
+
+    public static class Factory extends AbstractFieldFactory<Integer, IntegerRangeSelectFieldDefinition> {
+
+        public Factory(IntegerRangeSelectFieldDefinition definition, ComponentProvider componentProvider) {
+            super(definition, componentProvider);
+        }
+
+        @Override
+        protected Component createFieldComponent() {
+            return new CustomField<Integer>() {
+
+                private final ComboBox<Integer> comboBox = new ComboBox<>();
+                @Override
+                public Integer getValue() {
+                    return comboBox.getValue();
+                }
+
+                @Override
+                protected void doSetValue(Integer value) {
+                    comboBox.setValue(value);
+                }
+
+                @Override
+                protected Component initContent() {
+                    comboBox.setItems(IntStream.range(definition.getMin(), definition.getMax() + 1).boxed().toArray(Integer[]::new));
+                    return comboBox;
+                }
+            };
+        }
+
+    }
+
+}
