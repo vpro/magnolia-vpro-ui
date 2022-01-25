c9deb32577723f3eba716276aa16a0bfd8707e04
Michiel Meeuwissen <git@meeuw.org>
2022-01-25 08:55:22 +0100

    split of magnolia-vpro-ui-poms

--- magnolia-vpro-ui/src/main/java/nl/vpro/magnolia/ui/countryfield/CountrySelectFieldDefinition.java
+++ magnolia-vpro-ui/src/main/java/nl/vpro/magnolia/ui/countryfield/CountrySelectFieldDefinition.java
@@ -4,8 +4,12 @@ package nl.vpro.magnolia.ui.countryfield;
 import com.neovisionaries.i18n.CountryCode;
 import com.vaadin.data.ValueContext;
 import info.magnolia.ui.field.FieldType;
+import lombok.Getter;
+import lombok.Setter;
 import nl.vpro.i18n.Locales;
 import nl.vpro.magnolia.ui.enumfield.AbstractEnumFieldDefinition;
+import org.meeuw.i18n.countries.Country;
+import org.meeuw.i18n.regions.Region;
 
 /**
  * @author Michiel Meeuwissen
@@ -13,6 +17,10 @@ import nl.vpro.magnolia.ui.enumfield.AbstractEnumFieldDefinition;
  */
 @FieldType("countryField")
 public class CountrySelectFieldDefinition extends AbstractEnumFieldDefinition<CountryCode> {
+
+    @Setter
+    @Getter
+    Class<? extends Region> clazz = Country.class;
 
     public CountrySelectFieldDefinition() {
         super(CountryCode.class);
