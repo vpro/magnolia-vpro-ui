9210e8105ac0c4fdb9b38d2022dae9e897679255
Michiel Meeuwissen <git@meeuw.org>
2022-01-27 13:38:39 +0100

    No point in showing text if the icons contains it already.

--- magnolia-vpro-ui-poms/src/main/java/nl/vpro/magnolia/ui/poms/AgeRatingSelectFieldDefinition.java
+++ magnolia-vpro-ui-poms/src/main/java/nl/vpro/magnolia/ui/poms/AgeRatingSelectFieldDefinition.java
@@ -2,6 +2,10 @@ package nl.vpro.magnolia.ui.poms;
 
 import info.magnolia.ui.field.FieldType;
 
+import java.util.regex.Pattern;
+
+import com.vaadin.data.ValueContext;
+
 import nl.vpro.domain.media.AgeRating;
 import nl.vpro.magnolia.ui.enumfield.AbstractEnumFieldDefinition;
 
@@ -12,9 +16,19 @@ import nl.vpro.magnolia.ui.enumfield.AbstractEnumFieldDefinition;
 @FieldType("ageRatingField")
 public class AgeRatingSelectFieldDefinition extends AbstractEnumFieldDefinition<AgeRating> {
 
+    private static final Pattern ICON_SUFFICES = Pattern.compile("^[0-9_]+$");
     public AgeRatingSelectFieldDefinition() {
         super(AgeRating.class);
     }
+
+    @Override
+    protected String convertToPresentation(String value, ValueContext context) {
+        if (isUseIcons() && value != null && ICON_SUFFICES.matcher(value).matches()) {
+            return "";
+        }
+        return super.convertToPresentation(value, context);
+    }
+
 
 }
 
