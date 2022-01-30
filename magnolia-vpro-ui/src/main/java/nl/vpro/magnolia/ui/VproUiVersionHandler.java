733ef6186b99dc5e48895467133b0bf85ab913d9
Michiel Meeuwissen <git@meeuw.org>
2022-01-30 20:48:45 +0100

    version handler

---
+++ magnolia-vpro-ui/src/main/java/nl/vpro/magnolia/ui/VproUiVersionHandler.java
@@ -0,0 +1,27 @@
+package nl.vpro.magnolia.ui;
+
+import info.magnolia.module.DefaultModuleVersionHandler;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.BootstrapSingleResource;
+import info.magnolia.module.delta.Task;
+
+import java.util.*;
+
+import nl.vpro.magnolia.ui.regions.RegionsSelectFieldSupport;
+
+public class VproUiVersionHandler extends DefaultModuleVersionHandler {
+    @Override
+    protected final List<Task> getExtraInstallTasks(InstallContext installContext) {
+        if (RegionsSelectFieldSupport.HAS_WEBJARS_JAR) {
+            log.info("Detected webjars, make sure it is bypassed in mgnl filter");
+            return Arrays.asList(
+                new BootstrapSingleResource(
+                    "webjars",
+                    "Bypass webjars",
+                    "/setup/config.server.filters.bypasses.webjars.yaml")
+            );
+        } else {
+            return Collections.emptyList();
+        }
+    }
+}
